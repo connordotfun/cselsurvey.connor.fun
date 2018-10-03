e219a0f26031bea135996e41ed7370f02edd7e5d
Ethan Hunter <fc.spaceman@gmail.com>
2018-10-03 17:04:50 -0600

    everything is broken but kevin says I have to leave

--- src/App.elm
+++ src/App.elm
@@ -2,6 +2,7 @@ import Browser
 import Html exposing (Html, button, div, text)
 import Html.Attributes exposing (style, id)
 import Html.Events exposing (onClick)
+import Http
 
 
 main =
@@ -13,20 +14,47 @@ main =
     }
 
 
-type alias Model = String
+type alias Model = 
+  { currentSentence: String
+  , allSentences: List String
+  , nextIndex: Int
+  }
+
+type alias Sentences = 
+  { 
+    results: List String
+  }
 
 init : () -> (Model, Cmd Msg)
 init _=
   (" ", Cmd.none)
 
 
-type Msg = NewSentance
+type Msg
+  = GetNewSentence
+  | NewSentencesGenerated (Result Http.Error Sentences)
+
+
+
+
+shouldRequestNewSentences : Model -> Bool
+shouldRequestNewSentences model = (model.nextIndex == List.length model.allSentences)
+
+
+getNewSentences : () -> Cmd Msg
+getNewSentences =
+  Http.get "https://d48oi7sm4b.execute-api.us-east-1.amazonaws.com/default/csel-survey-generator"
+
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    NewSentance -> 
-      ("This is an example sentance", Cmd.none)
+    GetNewSentence -> 
+      if shouldRequestNewSentences model then
+        (model, )
+    NewSentencesGenerated newData ->
+      (model, Cmd.none)
+
 
 
 subscriptions : Model -> Sub Msg
@@ -41,5 +69,5 @@ view model =
         div [id "header"] [ text "What do students think about CSEL?" ]
         , text model
       ]
-    , button [id "button", onClick NewSentance ] [ text "Find Out!" ]
+    , button [id "button", onClick GetNewSentence ] [ text "Find Out!" ]
     ]
