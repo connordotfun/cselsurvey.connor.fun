f1de04891d67bc956e80a210804d5235d25789ba
Ethan Hunter <ethan.hunter@colorado.edu>
2018-10-02 21:52:22 -0600

    stuff

--- src/App.elm
+++ src/App.elm
@@ -1,37 +1,44 @@
-module App exposing (..)
-
-import Html exposing (Html, div, text)
-
-
--- MODEL
-
-
-type alias Model =
-    String
-
-
-init : ( Model, Cmd Msg )
-init =
-    ( "Hello", Cmd.none )
-
-
-type Msg
-    = NoOp
-
-view : Model -> Html Msg
-view model =
-    div []
-        [ text model ]
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        NoOp ->
-            ( model, Cmd.none )
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
 
 
 main =
+  Browser.element 
     { init = init
-    , view = view
     , update = update
+    , subscriptions = subscriptions
+    , view = view 
     }
+
+
+type alias Model = String
+
+init : (Model, Cmd Msg)
+init =
+  ("", Cmd.none)
+
+
+type Msg = Increment | Decrement
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Increment ->
+      (model ++ "", Cmd.none)
+
+    Decrement ->
+      (model ++ "", Cmd.none)
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ button [ onClick Decrement ] [ text "-" ]
+    , div [] [ text model ]
+    , button [ onClick Increment ] [ text "+" ]
+    ]
